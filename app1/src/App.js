75527120c14bd9864c1076df6871941a08d31af7
parekhnandini <parekhnandini201@gmail.com>
2024-05-03 00:23:25 +0530

    basics of react

---
+++ app1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          welcome to the world of react
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ app1/src/sample.js
@@ -0,0 +1,11 @@
+import React from"react";
+import ReactDom from "react-dom";
+class JSXDemo extends React.Component {
+render()
+{
+    return<p>Welcome to the world of React</p>
+
+}
+}
+
+exportDefault();
